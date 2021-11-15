c5e46ba3a77d56ca3240fc2003e74941da37d3a5
neilchawla <>
2021-11-15 18:45:30 +0000

    added session store

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var dashboardRouter = require('./routes/dashboard');
 var invalidLoginRouter = require('./routes/invalidLogin');
 
 var app = express();
-// var SQLiteStore = require('connect-sqlite3')(session);
+var SQLiteStore = require('connect-sqlite3')(session);
 
 require('./config/passport')(passport);
 
@@ -25,7 +25,9 @@ var session_config = {
   resave: true,
   saveUninitialized: true,
   cookie: { secure: true },
-  // store: new SQLiteStore
+  store: new SQLiteStore({
+    db: './database/task.sqlite'
+  })
   // store parameter when saving session to database
 };
 session_config.cookie.secure = false;
