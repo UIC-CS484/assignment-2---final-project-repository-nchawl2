6875b8b72b1b9a9485896cc4e55e3a96a12ef11b
neilchawla <>
2021-12-03 22:05:26 +0000

    minor updates

--- routes/livescoreDark.js
+++ routes/livescoreDark.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var today = new Date();
-var date = String(today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate());
+var date = new Date().toISOString().slice(0, 10);
+// var date = String(today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate());
 var api_key = "918f77748056929b5e2a60ceb6f4ad4e";
 
 /* GET home page. */
